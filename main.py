9ba86135e197aa95a240247fa0668e641aaee4dc
Shashwot90 <shashwotpradhan@gmail.com>
2025-05-20 05:35:08 +0545

    groq sys prompt

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 
 def get_sql_query_from_text(user_query):
-    
+    groq_sys_prompt = ChatPromptTemplate.from_template("""
+                    
