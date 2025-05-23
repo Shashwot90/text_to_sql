663ffacd6d3c34a18ac6d0fbb60ddf39016b5d85
Shashwot90 <shashwotpradhan@gmail.com>
2025-05-23 06:32:15 +0545

    ex1

--- main.py
+++ main.py
@@ -11,4 +11,5 @@ def get_sql_query_from_text(user_query):
                     The SQL database has the name STUDENT and has the following columns - NAME, COURSE, 
                     SECTION and MARKS. For example, 
                     Example 1 - How many entries of records are present?, 
+                        the SQL command will be something like this SELECT COUNT(*) FROM STUDENT;
                     
