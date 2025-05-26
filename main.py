b72224a31d2dc1fb6d0f638e8e68be975e674e14
Shashwot90 <shashwotpradhan@gmail.com>
2025-05-27 05:21:04 +0545

    llm

--- main.py
+++ main.py
@@ -20,4 +20,5 @@ def get_sql_query_from_text(user_query):
                     No preamble, only valid SQL please
                                                        """)
     model="llama3-8b-8192"
+    llm = ChatGroq(
     
